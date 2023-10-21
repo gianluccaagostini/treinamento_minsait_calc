408d595c739daa5735f64e92c25d6df355176f46
Gianlucca Agostini <gianluccaagostini97@gmail.com>
2023-10-21 10:47:22 -0300

    Inicio projeto

---
+++ src/main/java/br/com/gianlucca/AppCalculadora/AppCalculadoraApplication.java
@@ -0,0 +1,13 @@
+package br.com.gianlucca.AppCalculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCalculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCalculadoraApplication.class, args);
+	}
+
+}
