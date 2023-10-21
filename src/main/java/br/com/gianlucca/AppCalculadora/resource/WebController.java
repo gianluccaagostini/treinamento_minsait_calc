063975c616f4f5f2bb1b1172a5759dba5c213600
Gianlucca Agostini <gianluccaagostini97@gmail.com>
2023-10-21 11:10:32 -0300

    Primeiros cálculos

--- src/main/java/br/com/gianlucca/AppCalculadora/resource/WebController.java
+++ src/main/java/br/com/gianlucca/AppCalculadora/resource/WebController.java
@@ -4,13 +4,40 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebController {
 	
+	@GetMapping("/")
 	public String ShowForm(Model model) {
 		//adicionar uma lista de operações
 		model.addAttribute("modelOperacoes", List.of("Soma","Subtracao"));
 		return "form";
+	}
+	
+	@PostMapping("/")
+	public String handleFormSubmission(@RequestParam String modelOperacao, 
+									   @RequestParam String valor01,
+			                           @RequestParam String valor02,
+									   Model model) {
+		if(modelOperacao.isEmpty() || valor01.isEmpty() || valor02.isEmpty()) {
+			return null;
+			
+			String resposta = "";
+			switch (modelOperacao) {
+			case "Soma";
+				int resp = Integer.parseInt(valor01) + Integer.parseInt(valor02);
+				resposta = String.valueOf(resp);
+				break;
+			case "Subtracao";
+			int resp = Integer.parseInt(valor01) - Integer.parseInt(valor02);
+			resposta = String.valueOf(resp);
+			break;
+			}
+		}
+
 	}
 }
